e433f738e0dd073a08b6844499815295c67eded5
abhishek <purohitabhishek308@gmail.com>
2023-12-12 10:42:46 +0530

    first commit

---
+++ assets/data.js
@@ -0,0 +1,215 @@
+export const TeamCore =[
+     {
+          name: "DHRUV PEDNEKAR",
+          postion: "Captain",
+          img: "/assets/img/Team/Core Team/1.png"
+     },
+     {
+          name: "JONATHAN JOSEPH",
+          postion: "MANAGER",
+          img: "/assets/img/Team/Core Team/2.png"
+     },
+     {
+          name: "JASON RODRIGUES",
+          postion: "Chief Financial Officer ",
+          img: "/assets/img/Team/Core Team/3.png"
+     }
+]
+export const ChassisTeam =[
+     {
+          name: "Mathew Payapilly",
+          postion: "Chassis Head",
+          img: "/assets/img/Team/Chassis/Chassis/1.png"
+     },
+     {
+          name: "Adrian Nair",
+          postion: "Chassis Junior",
+          img: "/assets/img/Team/Chassis/Chassis/2.png"
+     },
+     {
+          name: "Mayank Katkar",
+          postion: "Chassis Junior",
+          img: "/assets/img/Team/Chassis/Chassis/3.png"
+     }
+     ,{
+          name: "Krish Karelia",
+          postion: "Chassis Junior",
+          img: "/assets/img/Team/Chassis/Chassis/4.png"
+     },
+     {
+          name: "Rudra Mhatre",
+          postion: "Chassis Junior",
+          img: "/assets/img/Team/Chassis/Chassis/5.png"
+     }
+]
+export const ElectronicsTeam = [
+     {
+          name:"Dhruv Pednekar",
+          postion:"Electronics Head",
+          img: "/assets/img/Team/Electronics/Electronics/1.png"
+     },
+     {
+          name:"Shahood Ansari",
+          postion:"Electronics Senior",
+          img: "/assets/img/Team/Electronics/Electronics/2.png"
+     },
+     {
+          name:"Malcolm Pereira",
+          postion:"Electronics Senior",
+          img: "/assets/img/Team/Electronics/Electronics/3.png"
+     },
+     {
+          name:"Smruti Bhatkar",
+          postion:"Electronics Senior",
+          img: "/assets/img/Team/Electronics/Electronics/4.png"
+     },
+     {
+          name:"Jacob Perumatil",
+          postion:"Electronics Senior",
+          img: "/assets/img/Team/Electronics/Electronics/5.png"
+     }
+
+]
+export const PowerTrain = [
+     {
+          name:"Mahender Choudhary",
+          position: "E-Powertrain Head",
+          img: "/assets/img/Team/E-powertrain/1.png"
+     },
+     {
+          name:"Yash More",
+          position: "E-Powertrain Senior",
+          img: "/assets/img/Team/E-powertrain/2.png"
+     },
+     {
+          name:"Jonathan Joseph",
+          position: "E-Powertrain Senior",
+          img: "/assets/img/Team/E-powertrain/3.png"
+     },
+     {
+          name:"Jason Rodrigues",
+          position: "E-Powertrain Senior",
+          img: "/assets/img/Team/E-powertrain/4.png"
+     },
+     {
+          name:"Shantanu Karekar",
+          position: "E-Powertrain Senior",
+          img: "/assets/img/Team/E-powertrain/5.png"
+     },
+     {
+          name:"Anoushka Dixit",
+          position: "E-Powertrain Junior",
+          img: "/assets/img/Team/E-powertrain/6.png"
+     },
+     {
+          name:"Vedika Vartak",
+          position: "E-Powertrain Junior",
+          img: "/assets/img/Team/E-powertrain/7.png"
+     },
+     {
+          name:"Vinay Pujari",
+          position: "E-Powertrain Junior",
+          img: "/assets/img/Team/E-powertrain/8.png"
+     },
+     {
+          name:"Yashraj Pandit",
+          position: "E-Powertrain Junior",
+          img: "/assets/img/Team/E-powertrain/9.png"
+     }
+]
+export const VehicleDynamics = [
+     {
+          name:"Amish Nair",
+          postion: "Vehicle Dynamics Head",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/1.png"
+     },
+     {
+          name:"Kunal Kore",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/2.png"
+     },
+     {
+          name:"Fahad Qureshi",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/3.png"
+     },
+     {
+          name:"Ishan Chatterjee",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/4.png"
+     },
+     {
+          name:"MUSAVIR",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/5.png"
+     },
+     {
+          name:"Harishankar Rajan",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/6.png"
+     },
+     {
+          name:"Chinmay Khadye",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/7.png"
+     },
+     {
+          name:"Pranav Dhole",
+          postion: "Vehicle Dynamics Senior",
+          img: "/assets/img/Team/Vehicle Dynamics/Vehicle Dynamics/8.png"
+     }
+]
+export const Sponsor = [
+     {
+          brandName: "Autoforms",
+          img: "/assets/img/Sponsers/autoforms.jpg"
+     },
+     {
+          brandName: "Comet",
+          img: "/assets/img/Sponsers/logo-comet-pumpen.jpg"
+     },
+     {
+          brandName: "Pankaj",
+          img: "/assets/img/Sponsers/PANKAJ POTENTIOMETERS.jpg"
+     },
+     {
+          brandName: "Simscale",
+          img: "/assets/img/Sponsers/simscale-logo.jpg"
+     },
+     {
+          brandName: "Mahabali",
+          img: "/assets/img/Sponsers/MM treatment.jpg"
+     },
+     {
+          brandName: "Prodigy kustoms",
+          img: "/assets/img/Sponsers/Prodigy Kustoms.jpeg"
+     },
+     {
+          brandName: "Svn laser tech",
+          //img: "/assets/img/Sponsers/.jpg"
+     },
+     {
+          brandName: "Metal form",
+          img: "/assets/img/Sponsers/MetalForm.jpg"
+     },
+     {
+          brandName: "Jk fibreglass",
+          img: "/assets/img/Sponsers/JK FIBREGLASS.png"
+     },
+     {
+          brandName: "Fibro chem",
+          img: "/assets/img/Sponsers/Fibrochem.png"
+     },
+     {
+          brandName: "Sushma gears",
+          // img: "/assets/img/Sponsers/.jpg"
+     },
+     {
+          brandName: "Ramani steel house",
+          img: "/assets/img/Sponsers/RAMANI STEEL HOUSE.jpeg"
+     },
+     {
+          brandName: "MC sparr",
+          // img: "/assets/img/Sponsers/.jpg"
+     }
+]
